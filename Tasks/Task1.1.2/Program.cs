96187a9613a77c32ebcf87f7765a3c8b5a7e5048
Иван <yufiv@inbox.ru>
2020-05-21 00:28:30 +0300

    Completed task 1.1.2

---
+++ Tasks/Task1.1.2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This task may be solved different variants
+            Console.WriteLine("Enter n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            //Variant 1: using a nested loop
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            //or
+            //Variant 2: using the creation a new line and filling it with asterisks
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+            Console.ReadKey();
+        }
+    }
+}

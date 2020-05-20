4906b4224fd0c9a495de9f8a3a232b26e87689d3
Иван <yufiv@inbox.ru>
2020-05-21 00:06:14 +0300

    Completed task 1.1.1

--- Tasks/Task1/Program.cs
+++ Tasks/Task1/Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Task1
+namespace Task111
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("asdasd");
+            Console.WriteLine("Enter sides a и b:");
+            string entered_line = Console.ReadLine();
+            string[] entered_numbers = entered_line.Split(' ');
+            try
+            {
+                int a = Convert.ToInt32(entered_numbers[0]);
+                int b = Convert.ToInt32(entered_numbers[1]);
+                if (a > 0 && a <= 9 && b > 0 && b <= 9)
+                    Console.WriteLine("Rectangle area with sides a and b = " + (a * b));
+                else
+                    Console.WriteLine("You entered incorrect values");
+            }
+            catch { }
+            Console.ReadKey();
         }
     }
 }

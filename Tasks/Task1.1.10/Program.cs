6e8f3ac7cd4998f78c3cb7fc958c1dfc3cc408bf
Иван <yufiv@inbox.ru>
2020-05-23 14:37:43 +0300

    Completed 1.1.10

---
+++ Tasks/Task1.1.10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task1._1._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int length_arr = 3;
+            int[,] arr = new int[length_arr, length_arr];
+            Random r = new Random();
+            Console.WriteLine("Source array: ");
+            int sum = 0;
+            for (int i = 0; i < length_arr; i++)
+            {
+                for (int j = 0; j < length_arr; j++)
+                {
+                    arr[i, j] = r.Next(100);
+                    Console.Write(arr[i, j] + " ");
+                    if ((i + j + 2) % 2 == 0)
+                        sum += arr[i, j];
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Sum: " + sum);
+            Console.ReadKey();
+        }
+    }
+}

b0fa2ab002a87302b6719c5b5b1b2cf898185379
Иван <yufiv@inbox.ru>
2020-05-23 14:28:44 +0300

    Completed task 1.1.9

---
+++ Tasks/Task1.1.9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1._1._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int length_arr = 10;
+            int[] arr = new int[length_arr];
+            Random r = new Random();
+            Console.WriteLine("Source array: ");
+            int sum = 0;
+            for (int i = 0; i < length_arr; i++)
+            {
+                arr[i] = r.Next(-100, 100);
+                if (arr[i] > 0)
+                    sum += arr[i];
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n\nNon-negative sum: " + sum);
+            Console.ReadKey();
+        }
+    }
+}

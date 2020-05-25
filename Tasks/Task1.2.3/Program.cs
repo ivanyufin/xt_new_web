c57d3a755db58a160c6384fa27da445f003df0b3
Иван <yufiv@inbox.ru>
2020-05-25 13:51:45 +0300

    Completed task 1.2.3

---
+++ Tasks/Task1.2.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            //Replace punctuation characters on space
+            string[] punk_marks = { ".", ",", ";", ":", "!", "?", "...", "-", "[", "]", "{", "}", "(", ")", "\"", "<", ">", "\'" };
+            for (int i = 0; i < punk_marks.Length; i++)
+                input = input.Replace(punk_marks[i], " ");
+
+            string[] arr = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach(var s in arr)
+                if (s[0] >= 'a' && s[0] <= 'z' || s[0] >= 'а' && s[0] <= 'я')
+                    count++;
+
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}

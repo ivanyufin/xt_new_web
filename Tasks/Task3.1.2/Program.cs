8bc25c437591c22fda231f7dcf27694101f7f2a8
Иван <yufiv@inbox.ru>
2020-07-12 15:45:23 +0300

    Completed task 3.1.2

--- Tasks/Task3.1.2/Program.cs
+++ Tasks/Task3.1.2/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task3._1._2
 {
@@ -10,31 +8,56 @@ namespace Task3._1._2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter text: ");
             string input = Console.ReadLine();
             //Replace punctuation characters on space
-            //Убираем все знаки пунктуации
-            string[] punk_marks = { ".", ",", ";", ":", "!", "?", "...", "-", "[", "]", "{", "}", "(", ")", "\"", "<", ">", "\'" };
-            for (int i = 0; i < punk_marks.Length; i++)
-                input = input.Replace(punk_marks[i], " ");
-
-            //Разбиваем строку на слова
+            ReplacePunctuation(input);
+            //Splits a string into words
             string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            //Создаем коллекцию, где слова и количество их повторений
+            //Create a collection where the words and the number of repetitions
             Dictionary<string, int> numberOfRepetitions = new Dictionary<string, int>();
+            bool diversity = true;
+            string MaxRepetitions = "";
             for (int i = 0; i < words.Length; i++)
             {
-                if(numberOfRepetitions.ContainsKey(words[i]))
+                if (numberOfRepetitions.ContainsKey(words[i]))
+                {
                     numberOfRepetitions[words[i]]++;
+                    if (PercentageOfTotal(numberOfRepetitions[words[i]], words.Length) >= 20.0f && diversity)
+                    {
+                        diversity = false;
+                        MaxRepetitions = words[i];
+                    }
+                }
                 else
                     numberOfRepetitions.Add(words[i], 1);
             }
+
+            Console.WriteLine();
+            if(diversity)
+                Console.WriteLine("Congratulations! You are well done, the text is diverse");
+            else
+                Console.WriteLine("The text is not diverse. Too many repetitions of the same word, namely: " + MaxRepetitions);
             //Выводим количество повторений для каждого слова
-            for(int i = 0; i < numberOfRepetitions.Count; i++)
-            {
-                Console.WriteLine("Количество повторений слова \"" + numberOfRepetitions.Keys.ElementAt(i) + "\" = " + numberOfRepetitions.Values.ElementAt(i));
-            }
+            Console.WriteLine();
+            Console.WriteLine("Number of repetitions of a word and percentage of total count words: ");
+            for (int i = 0; i < numberOfRepetitions.Count; i++)
+                Console.WriteLine(numberOfRepetitions.Keys.ElementAt(i) + "\" = " + numberOfRepetitions.Values.ElementAt(i) + " " + PercentageOfTotal(numberOfRepetitions.Values.ElementAt(i), words.Length).ToString("F2") + "%");
 
             Console.ReadKey();
+        }
+
+        static double PercentageOfTotal(int element, int count)
+        {
+            return (((double)element / (double)(count)) * 100);
+        }
+
+        static string ReplacePunctuation(string input)
+        {
+            string[] punk_marks = { ".", ",", ";", ":", "!", "?", "...", "-", "[", "]", "{", "}", "(", ")", "\"", "<", ">", "\'" };
+            for (int i = 0; i < punk_marks.Length; i++)
+                input = input.Replace(punk_marks[i], " ");
+            return input;
         }
     }
 }

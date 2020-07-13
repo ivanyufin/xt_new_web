29dcdf0bf45a1e4a34d4e75c60828f31342c0e34
Иван <yufiv@inbox.ru>
2020-07-13 17:44:10 +0300

    Completed task 3.2.1

---
+++ Tasks/Task3.2.1/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray<int> array = new DynamicArray<int>();
+            for (int i = 0; i < 8; i++)
+                array.Add(i);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class DynamicArray<T> : IEnumerable<T>
+    {
+        private T[] array;
+
+        public DynamicArray()
+        {
+            array = new T[8];
+            capacity = 8;
+        }
+
+        public DynamicArray(int capacity)
+        {
+            array = new T[capacity];
+        }
+
+        public DynamicArray(IEnumerable<T> collection)
+        {
+            array = collection.ToArray();
+        }
+
+        public void Add(T item)
+        {
+            if (length < capacity)
+            {
+                array[length] = item;
+                length++;
+            }
+            else
+            {
+                capacity = capacity * 2;
+                T[] newArray = new T[capacity];
+                for (int i = 0; i < array.Length; i++)
+                    newArray[i] = array[i];
+                newArray[length] = item;
+                length++;
+                array = newArray;
+            }
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if(length + collection.Count() >= capacity)
+            {
+                while(length + collection.Count() >= capacity)
+                    capacity = capacity * 2;
+                T[] newArray = new T[capacity];
+                for (int i = 0; i < array.Length; i++)
+                    newArray[i] = array[i];
+                for (int i = 0; i < collection.Count(); i++)
+                    newArray[i + length] = collection.ElementAt(i);
+                length += collection.Count();
+                array = newArray;
+            }
+            else
+            {
+                T[] newArray = new T[capacity];
+                for (int i = 0; i < array.Length; i++)
+                    newArray[i] = array[i];
+                for (int i = 0; i < collection.Count(); i++)
+                    newArray[i + length] = collection.ElementAt(i);
+                length += collection.Count();
+                array = newArray;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            bool removed = false;
+
+            T[] newArray = new T[capacity];
+            int j = 0;
+            for (int i = 0; i < length; i++)
+                if (!array[j].Equals(item))
+                {
+                    newArray[i] = array[j];
+                    j++;
+                }
+                else
+                {
+                    if (j + 1 < length)
+                    {
+                        j++;
+                        newArray[i] = array[j];
+                    }
+                    else
+                        newArray[i] = default(T);
+                    length--;
+                    removed = true;
+                }
+            array = newArray;
+            return removed;
+        }
+
+        private int length;
+        public int Length
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+        private int capacity;
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        public bool Insert(T item, int index)
+        {
+            bool inserted = false;
+
+            if (index < 0 || index >= capacity)
+                throw new ArgumentOutOfRangeException();
+
+            T[] newArray = new T[capacity + 1];
+            int j = 0;
+            for (int i = 0; i < length; i++)
+                if (i != index)
+                {
+                    newArray[i] = array[j];
+                    j++;
+                }
+                else
+                {
+                    if (length >= capacity)
+                        capacity = capacity * 2;
+                    newArray[i] = item;
+                    inserted = true;
+                    length++;
+                }
+
+            array = newArray;
+            return inserted;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= capacity)
+                    throw new ArgumentOutOfRangeException();
+                return array[index];
+            }
+            set
+            {
+                if (index < 0 || index >= capacity)
+                    throw new ArgumentOutOfRangeException();
+                array[index] = value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for(int i = 0; i < length; i++)
+                yield return array[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+    }
+}
---
+++ Tasks/Task3.2.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Task3.2.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Task3.2.1")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6d1647dd-5370-4ee7-98d4-4357e887493d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
